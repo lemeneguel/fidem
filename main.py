902cf09e156b1f01992e00f7c9fe865941e32c05
lemeneguel <lecomp@icloud.com>
2020-02-16 18:44:37 -0500

    adding ration to the form

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return 'Hello World!'
+
 @app.route("/form/<name>")
 def form(name):
     return render_template("form.html", name=name)
+
+@app.route("/shopping")
+def shopping():
+    food = ["Cheese", "Tuna", "Beef"]
+    return render_template("shopping.html", food=food)
 
 if __name__ == "__main__":
     app.run()
